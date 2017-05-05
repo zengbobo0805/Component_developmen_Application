5145825437b0152c84898ec21688f3d9b68a50fe
zengbobo0805 <boy812796958@aliyun.com>
2017-05-05 11:00:54 +0800

    1.0.0 lib

---
+++ common_lib/src/main/java/com/component/developmen/commonLib/L.java
@@ -0,0 +1,13 @@
+package com.component.developmen.commonLib;
+
+import android.util.Log;
+
+/**
+ * Created by bobo on 2017/5/5.
+ */
+
+public class L {
+    public static void i(String msg){
+        Log.i("component.developmen",msg);
+    }
+}
