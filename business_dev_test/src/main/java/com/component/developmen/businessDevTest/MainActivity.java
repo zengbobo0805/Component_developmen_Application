3dcde78349f1d91b1426abda1fd0480210c5c4c3
zengbobo0805 <boy812796958@aliyun.com>
2017-05-05 11:31:30 +0800

    1.0.0 业务开发调试

---
+++ business_dev_test/src/main/java/com/component/developmen/businessDevTest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.component.developmen.businessDevTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dev_test_activity_main);
+    }
+}
